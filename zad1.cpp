57d5cd2c87910a670f1d2fa4449cfd8589d83ea8
lewy700 <bartosztrojanofficial@gmail.com>
2024-10-18 04:58:24 +0200

    zad1 + tasks content

--- zad1.cpp
+++ zad1.cpp
@@ -1,16 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string island[501];
+bool visited[501][501];
 
+int n,m;
 
-int main(){	
-	int m;
-	int n;
+void take_a_walk(int i, int j){
+	visited[i][j] = 1;
 	
+	if (j!=0 && visited[i][j-1] == 0 && island[i][j-1] == 'l') take_a_walk(i, j-1);
+	if (j!=n-1 && visited[i][j+1] == 0 && island[i][j+1] == 'l') take_a_walk(i, j+1);
+	if (i!=0 && visited[i-1][j] == 0 && island[i-1][j] == 'l') take_a_walk(i-1, j);
+	if (i!=m-1 && visited[i+1][j] == 0 && island[i+1][j] == 'l') take_a_walk(i+1, j);
+}
+
+int main(){		
 	cin >> m >> n;
-	
-	string island[m];
-	bool visited[m][n];
 	
 	for(int i=0;i<m;i++){
 		cin >> island[i];
@@ -19,15 +25,6 @@ int main(){
 		for(int j=0;j<n;j++){
 			visited[m][n] = 0;
 		}
-	}
-	
-	void take_a_walk(int i, int j){
-		visited[i][j] = 1;
-		
-		if (j!=0 && visited[i][j-1] == 0 && island[i][j-1] == 'l') take_a_walk(i, j-1);
-		if (j!=n-1 && visited[i][j+1] == 0 && island[i][j+1] == 'l') take_a_walk(i, j+1);
-		if (i!=0 && visited[i-1][j] == 0 && island[i-1][j] == 'l') take_a_walk(i-1, j);
-		if (i!=m-1 && visited[i+1][j] == 0 && island[i+1][j] == 'l') take_a_walk(i+1, j);
 	}
 	
 	for(int i=0;i<n;i++){
@@ -48,6 +45,14 @@ int main(){
 	}
 	
 	cout << counter;
+//	for(int b=0;b<500;b++){
+//		string a="";
+//		for(int b=0;b<250;b++){
+//			a += "sl";
+//		}
+//		cout << a << endl;
+//	}
+	
 	
 	return 0;
 }
