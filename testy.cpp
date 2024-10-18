1bbf39b9bfa7f17f5bf8f172a1e69e611b9cce31
lewy700 <bartosztrojanofficial@gmail.com>
2024-10-18 05:45:59 +0200

    small fixes

---
+++ testy.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	for(int i=0;i<500; i++){
+		cout << 2000 << " ";
+	}
+	cout << endl;
+	for(int i=0;i<500; i++){
+		cout << 2000 << " ";
+	}
+	return 0;
+}
--- zad2.cpp
+++ zad2.cpp
@@ -19,11 +19,13 @@ int main(){
 	int counter = 0;
 
 	for(int i=0;i<n;i++){
+		cout << i << endl;
 		if(i==0 && squares[i] == 'b'){ flips[i] = 1; counter++;}
 		else if(i==0) continue;
 		else if(i<k){
 			if((squares[i] == 'b' && flips[i-1]%2!=1) || (squares[i] == 'w' && flips[i-1]%2==1)){
-				flips[i] = flips[i-1] + 1;
+				if(flips[i-1] + 1 < 4) flips[i] = flips[i-1] + 1;
+				else flips[i] = 0;
 				counter++;
 				
 				if(i>(n-k)){
